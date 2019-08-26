f0c2d651fd466d216022336f3a27cb4a0dc77b25
wangyi <724003548@qq.com>
2019-08-26 12:04:19 +0800

    1

--- packages/antdsite/lib/plugins/gatsby-remark-antd-copmonents/__test__/index.js
+++ packages/antdsite/lib/plugins/gatsby-remark-antd-copmonents/__test__/index.js
@@ -1,6 +1,7 @@
 var unified = require('unified');
 var reParse = require('remark-parse');
 var remark2rehype = require('remark-rehype');
+var stringify = require('rehype-stringify');
 
 const render = (text, allowTitle) =>
   unified()
@@ -9,4 +10,8 @@ const render = (text, allowTitle) =>
     .use(stringify)
     .processSync(text);
 
-render('`a`');
+var a = render(`
+    # asdasd
+> 232323    \`a\`
+`);
+console.log(a);
